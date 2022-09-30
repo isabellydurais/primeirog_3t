b0f6607792e521bf3321a5446fb192e179d4369a
isabellydurais <108758458+isabellydurais@users.noreply.github.com>
2022-09-30 12:46:31 -0300

    Aula 30/09

--- estudo.js
+++ estudo.js
@@ -1,1 +1,18 @@
+let tabuada = 5;
+
+function escreva (){
+    document.write ("Tabuada do " + tabuada + "<br>")
+    document.write(tabuada + " x 1 =  " + (tabuada*1)+ "<br>");
+    document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
+    document.write(tabuada + " x 3 =  " + (tabuada*3)+ "<br>");
+    document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
+    document.write(tabuada + " x 5 =  " + (tabuada*5)+ "<br>");
+    document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
+    document.write(tabuada + " x 7 =  " + (tabuada*7)+ "<br>");
+    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
+    document.write(tabuada + " x 9 =  " + (tabuada*9)+ "<br>");
+    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
+
+}
+
 
