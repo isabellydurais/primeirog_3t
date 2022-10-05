ccca9ca139cab03e5d41a6f8a183a27d895e359c
isabellydurais <108758458+isabellydurais@users.noreply.github.com>
2022-10-05 11:56:47 -0300

    Aula 05/10

--- estudo.js
+++ estudo.js
@@ -15,4 +15,21 @@ function escreva (){
 
 }
 
+let lista = ["Terra", "Marte", "Saturno" , "Júpiter" , "Vênus" , "Mercúrio"];
+function mostraLista (){
+    document.write ("Tamanho da lista: " + lista.length + "<br>");
+    
+    for(let i = 0; i < lista.length; i++){
+        document.write ("Planeta " + lista[i] + "<br>");
+    }
+}
 
+function repete (){
+    for(let i = 1; i < 10; i ++){
+        document.write("Tabuada do " + i + "<br>");
+        for(let j = 1; j <= 10; j ++){
+            document.write(i + " x " + j + " = " + (j*j) + "<br>");
+
+        }
+    }
+}
