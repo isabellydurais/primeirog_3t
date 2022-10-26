5c82f595fc8796561bed949cd6d0d67732d0730a
isabellydurais <108758458+isabellydurais@users.noreply.github.com>
2022-10-26 11:55:53 -0300

    Aula 26/10

--- estudo.js
+++ estudo.js
@@ -1,7 +1,25 @@
 function total(){
-    let c = document.getElementById("valor").value;
+    let c = document.getElementById("capital").value;
     let j = document.getElementById("juros").value;
     let t = document.getElementById("meses").value;
+    if(!Number(c)){
+      alert("O valor do capital deve ser um número.");
+      document.getElementById("capital").value = "";
+      document.getElementById("capital").focus();
+      return
+    }
+    if(!Number(j)){
+      alert("O valor dos juros deve ser um número.");
+      document.getElementById("juros").value = "";
+      document.getElementById("juros").focus();
+      return
+    }
+    if(!Number(t)){
+      alert("A quantidade de meses deve ser um número.");
+      document.getElementById("meses").value = "";
+      document.getElementById("meses").focus();
+      return
+    }
     let r = 0;
     for(let i = 1; i <= t; i++){
         r = c * (1 + (j/100));
@@ -9,8 +27,7 @@ function total(){
         c = r;
     }
     document.write("Resultado: " + r);
-  }
-  
+}
   
   function adicao(){
   let val1 = document.getElementById("v1").value;
@@ -47,4 +64,6 @@ function total(){
           let r = Number(val1) * Number(p);
           document.getElementById("resultado").innerHTML =r;
           }
+    
+
     
