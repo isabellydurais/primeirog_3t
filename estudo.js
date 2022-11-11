13f0c8fa2ebf1ea2f81ff84555a82541262d2f91
isabellydurais <108758458+isabellydurais@users.noreply.github.com>
2022-11-11 12:46:18 -0300

    11/11

--- estudo.js
+++ estudo.js
@@ -71,6 +71,24 @@ function total(){
           let r = Number(val1) * Number(p);
           document.getElementById("resultado").innerHTML = r;
           }
-    
+          function calculaRaiz (){
+            let a = document.getElementById("a").value;
+            let b = document.getElementById("b").value;
+            let c = document.getElementById("c").value;
+            let delta = (b*b) - (4*a*c);
+            let raiz, x1, x2;
 
+            if(delta>=0){
+              raiz= Math.sqrt(delta);
+              x1 = ((-b)+raiz)/ (2*a);
+              x2 = ((-b)-raiz)/ (2*a);
+              raiz = "x1 ="+x1+" x2="+x2;
+            }else{
+              raiz= "Não possui raiz Real";
+
+            }
+
+            document.getElementById("raiz").innerHTML = raiz;
+            
+          }
     
